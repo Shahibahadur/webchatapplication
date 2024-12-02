53f17b3c7dbdbbd1880de670028e8ff16df140b7
Shahibahadur <163428741+Shahibahadur@users.noreply.github.com>
2024-12-02 18:44:00 +0545

    Update GroupJoinRequestsServlet.java
    user_name chaneg to fname, lname here

--- src/main/java/group/GroupJoinRequestsServlet.java
+++ src/main/java/group/GroupJoinRequestsServlet.java
@@ -38,7 +38,7 @@ public class GroupJoinRequestsServlet extends HttpServlet {
         ArrayList<Map<String, String>> joinRequests = new ArrayList<>();
 
         try (Connection connection = new DatabaseConfig().getConnection()) {
-            String sql = "SELECT r.request_id, r.user_id, u.user_name, g.group_name "
+            String sql = "SELECT r.request_id, r.user_id, u.fname, u.lname, g.group_name "
                     + "FROM group_join_requests r "
                     + "JOIN users u ON r.user_id = u.unique_id "
                     + "JOIN `groups` g ON r.group_id = g.group_id "
@@ -50,7 +50,8 @@ public class GroupJoinRequestsServlet extends HttpServlet {
                         Map<String, String> requests = new HashMap<>();
                         requests.put("request_id", rs.getString("request_id"));
                         requests.put("user_id", rs.getString("user_id"));
-                        requests.put("user_name", rs.getString("user_name"));
+                        requests.put("user_fname", rs.getString("fname"));
+                         requests.put("user_fname", rs.getString("lname"))
                         requests.put("group_name", rs.getString("group_name"));
                         joinRequests.add(requests);
                     }
