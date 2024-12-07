482b3a22dcc5796095a443bb78f8154b66f98362
Shahibahadur <163428741+Shahibahadur@users.noreply.github.com>
2024-12-07 16:26:37 +0545

    Update groupmesage.js
    group_id is accessed using dataset here

--- src/main/webapp/js/groupmesage.js
+++ src/main/webapp/js/groupmesage.js
@@ -1,9 +1,10 @@
 async function fetchMessages(){
 	const messageContainer = document.getElementById("messageContainer");
+	let id = messageConatiner.dataset.group_id;
 	messageContainer.innerHTML="";
 	
 	try{
-		const response = await fetch("/ChattAPP/GetMessage",{
+		const response = await fetch(`/ChattAPP/GetMessage?group_id={encodeURIComponent(id)}`,{
 			
 			method : "POST",
 			headers: {
@@ -14,11 +15,11 @@ async function fetchMessages(){
 		
 		if(response.ok){
 			const messages = await response.json();
-			let i=0;
+			
 			messages.forEach((message)=>{
 				
 				const messageElement = document.createElement("div");
-				messageElement.classList.add(`message${i++}`);
+				messageElement.classList.add("message");
 				messageElement.innerHTML=`
 				<strong>${message.senderName}</strong>
 				<span>${new Date(message.timestamp).toLocaleString()}</span>
