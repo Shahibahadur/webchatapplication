7a0861ad70ce4d9609b3dea1bb34769b397781f3
Shahibahadur <163428741+Shahibahadur@users.noreply.github.com>
2024-12-17 21:56:16 +0545

    Update DeleteUser.java
    code is written

--- src/main/java/admin/DeleteUser.java
+++ src/main/java/admin/DeleteUser.java
@@ -26,7 +26,23 @@ public class DeleteUser extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String uniqueId = (String)request.getParameter("uniqueId");
+		if(!uniqueId || uniqueId.isBlank()){
+			response.setStatus(HttpServletResponse_SC_BAD_REQUEST);
+		}
+		try(Connection conn = new DatabaseConfig().getConnection();
+		PreparedStatement pstmt = conn.prepareStarement("DELETE FROM users WHERE unique_id = 'uniqueId'");){
+			int i = pstmt.executeUpdate();
+			if(i>0){
+				response.setStatus(HttpServletResponse.SC_OK);
+			}else{
+				response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR));
+		}
+		
 	}
 
 	/**
